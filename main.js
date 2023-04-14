fdbaf9ddd415b892179c21761a01a2450a0ed8a0
SwapnanilGhosh <swapnanil2002@gmail.com>
2023-04-14 15:02:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
-var web3 = window.web3 ? new Web3(window.web3.currentProvider) : new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/4d8aa2ec027a40e2a3ed9d14aedf7767"));
-
-
+var web3 = window.web3 ?
+  new Web3(window.web3.currentProvider) :
+  new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/4d8aa2ec027a40e2a3ed9d14aedf7767"));
 
 
 
